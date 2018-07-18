e996b787279bb9a5551a96d1c5018a3f5fb1eb6e
Brandon Green <brandon@brandonleeproductions.com>
2018-07-18 23:45:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,12 +11,11 @@ class Student
     #binding.pry
     # @student_hash = student_hash
     student_hash.each {|key, value| self.send(("#{key}="), value)}
-
     @@all << self
   end
 
   def self.create_from_collection(students_array)
-
+    @name = name
   end
 
   def add_student_attributes(attributes_hash)
