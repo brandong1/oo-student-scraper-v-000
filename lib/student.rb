ebc6e0c50176b970a9996dc5ef40bf27eeab00e2
Brandon Green <brandon@brandonleeproductions.com>
2018-07-19 00:18:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
+    students_array.detect do |student|
       #binding.pry
 
       student = student[:name]
